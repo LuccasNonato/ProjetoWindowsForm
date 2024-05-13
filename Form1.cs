7478d69f48277e0a21fd762601bb2ddb45584105
Luccas <luccasjunqueira@hotmail.com>
2024-05-12 23:54:25 -0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProjetoWindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //string texto = "mensagem do botao";
+        minhabiblioteca.MinhaClasse minhaClasse = new minhabiblioteca.MinhaClasse();  
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(minhaClasse.texto);
+        }
+    }
+}
+
+
+// classe basica inicial
+namespace minhabiblioteca
+{
+    public class MinhaClasse
+    {
+        public string texto = "mensagem botão 2";
+    }
+}
